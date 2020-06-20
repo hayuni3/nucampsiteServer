026cb3c70eee2c7d733ee85cbde36054d5db69f8
Hayat Ibrahim <hayusnn@gmail.com>
2020-06-20 02:21:52 -0500

    OAuth and User Authentication with Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '572657740351112',
+        clientSecret: 'e41deb2430d6559e8324fd52c12c7d7e'
+    }
 }
